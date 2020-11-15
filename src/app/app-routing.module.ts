bad16e972990f9c0c73335b6f2f30a3fc6a3f5ee
nadineuwineza <nadineuwineza2017@gmail.com>
2020-11-15 20:59:23 +0200

    importing nav and creating a path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NavComponent} from '../app/nav/nav.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'nav', component: NavComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
