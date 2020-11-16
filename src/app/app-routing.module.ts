1a01e5e1bbaf4078f24a86a25ddb0614feed8bcf
nadineuwineza <nadineuwineza2017@gmail.com>
2020-11-16 10:49:17 +0200

    add about

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { NavComponent} from '../app/nav/nav.component'
+import { AboutComponent} from '../app/about/about.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { FormComponent} from '../app/form/form.component'
 
 const routes: Routes = [
-  { path: 'nav', component: NavComponent},
+  { path: 'form',component:FormComponent},
+  { path: 'about', component: AboutComponent},
+  { path: '', redirectTo: "/form", pathMatch: "full" },
+  
+  { path:'**', component:NotFoundComponent},
+ 
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { FormComponent } from './form/form.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    FormComponent
+    FormComponent,
+    NotFoundComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
